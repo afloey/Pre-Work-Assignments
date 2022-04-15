ddaa479a8781ad141a33cb13ab4d7e5f5c51e542
Ashley <alflorimonte@gmail.com>
2022-04-14 20:12:40 -0700

    first commit

--- Prework.py
+++ Prework.py
@@ -1,5 +1,6 @@
 #Question 1: Write a function to print "hello_USERNAME!" USERNAME is the input of the function. 
 # The first line of the code has been defined as below.
+from ast import If, Return, While
 import numbers
 from string import digits
 
@@ -11,23 +12,21 @@ hello_name('ashley')
 
 
 #Question 2: Write a python function, first_odds that prints the odd numbers from 1-100 and returns nothing.
-def first_odds(odd_numbers):
-    odd_numbers = list(range(1,100))
-    return odd_numbers(0)
-
-print(first_odds)
-
+def first_odds():
+    odd_numbers = list(range(1,100,2))
+    print(odd_numbers)
+    
+first_odds()
 
 #Question 3: Please write a Python function, max_num_in_list to return the max number of a given list. 
 # The first line of the code has been defined as below.
 
 def max_num_in_list(a_list):
-    """Return the max number in a given list."""
-    a_list= (1,3,8,12,35)
-    for list in a_list:
-            max(a_list)
+    """Return the max number in a given list"""
+    max_value = max(a_list)
+    return(max_value)
 
-print(max_num_in_list)   
+print(max_num_in_list([1,3,8,12,35]))
 
 
 #Question 4: Write a function to return if the given year is a leap year. 
@@ -36,15 +35,22 @@ print(max_num_in_list)
 
 def is_leap_year(a_year):
     """Return if the given year is a leap year."""
-   # a_year % 4 = True and a_year
-   # is_leap_year = % 4
-    #is_leap_year != % 100 unless % 400 
+    year_value = a_year
+    a_year % 4 == 0 and a_year % 400 == 0
+    if a_year == int:
+        print('True')
 
-#return = True 
+print(is_leap_year(2022))
 
 
 #Question 5: Write a function to check to see if all numbers in list are consecutive numbers. 
 # For example, [2,3,4,5,6,7] are consecutive numbers, but [1,2,4,5] are not consecutive numbers. 
 # The return should be boolean Type.
+
+def is_consecutive(a_list):
+    num_list= a_list.sort()
+    print(a_list)
+
+print(is_consecutive([1,3,2,7]))
 
 
